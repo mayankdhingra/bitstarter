b93953e0f82eb5c1e9bc7c1b86b8972b73cfbca7
mayankdhingra <dhingra.mayank@gmail.com>
2013-07-21 04:52:08 +0000

    reading from disk

--- web.js
+++ web.js
@@ -3,7 +3,14 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldlings!');
+fs.readFileSync('./index.html',function read(err, data){
+	if (err) {
+		throw err;
+	}
+	content = data;
+	response.send(content);
+
+) 
 });
 
 var port = process.env.PORT || 5000;
