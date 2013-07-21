09148855f1eab6c0c69acf8f3084f8560a7cdf66
mayankdhingra <dhingra.mayank@gmail.com>
2013-07-21 05:18:02 +0000

    reading from disk2

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-
+var fs = require("fs");
 
 app.get('/', function(request, response) {
+var text = fs.readFileSync('hello.html','utf8')
 
-	response.send('Hello');
+	response.send(text);
 
  
 });
